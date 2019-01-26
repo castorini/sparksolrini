ade3035114ee526290b140a909f50c004e7d52db
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-01-26 07:33:17 -0500

    Intermediate run script update

--- src/main/scala/ca/uwaterloo/Constants.scala
+++ src/main/scala/ca/uwaterloo/Constants.scala
@@ -1,6 +1,7 @@
 package ca.uwaterloo
 
 object Constants {
-	val MILLIS_IN_DAY = 1000 * 60 * 60 * 24
+	val MILLIS_IN_MINUTE = 1000 * 60
+	val MILLIS_IN_DAY = MILLIS_IN_MINUTE * 60 * 24
 	val MAX_ROW_PER_QUERY = 2147483622
 }
--- src/main/scala/ca/uwaterloo/SIGIR/task/SleepTask.scala
+++ src/main/scala/ca/uwaterloo/SIGIR/task/SleepTask.scala
@@ -1,7 +1,9 @@
 package scala.ca.uwaterloo.SIGIR.task
 
+import ca.uwaterloo.Constants.MILLIS_IN_MINUTE
+
 class SleepTask(duration: Long) extends Task {
   def process(content: String) {
-    Thread.sleep(duration)
+    Thread.sleep(duration * MILLIS_IN_MINUTE)
   }
 }
