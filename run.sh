9db5ad160a75a0b41a7737f1c22a96cabe1465bb
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-01-26 07:34:04 -0500

    Intermediate run script update

--- run.sh
+++ run.sh
@@ -8,22 +8,32 @@ export HADOOP_CONF_DIR=/localdisk5/hadoop/hadoop/etc/hadoop
 export SPARK_HOME=/localdisk5/hadoop/spark
 export LD_LIBRARY_PATH=/localdisk5/hadoop/hadoop/lib/native:$LD_LIBRARY_PATH
 
-: '
-spark-submit \
-    --name sent-detector-parallel-spark \
-    --class ca.uwaterloo.SIGIR.ParallelDocIdSpark \
-    --num-executors 9 --executor-cores 16 --executor-memory 48G --driver-memory 32G \
-    target/cs848-project-1.0-SNAPSHOT.jar \
-    --term $2 \
-    --field raw \
-    --solr 192.168.1.111:9983 \
-    --index $1 \
-    --task $3
-'
+# Term list
+declare -a terms=("dog") # "cat" "horse" "goose")
+
+for d in 3 # 9 27
+do
+    for t in terms
+    do
+        # Task 1
+        spark-submit \
+        --name parallel-docid-spark \
+        --class ca.uwaterloo.SIGIR.ParallelDocIdSpark \
+        --num-executors 9 --executor-cores 16 --executor-memory 48G --driver-memory 32G \
+        target/cs848-project-1.0-SNAPSHOT.jar \
+        --term $2 \
+        --field raw \
+        --solr 192.168.1.111:9983 \
+        --index $1 \
+        --task $3 \
+        --duration d \
+        &> "parallel-docid-spark-" + t + "-" + d
+    done
+done
 
 : '
 spark-submit \
-    --name sent-detector-hdfs-spark \
+    --name hdfs-spark \
     --class ca.uwaterloo.SIGIR.HdfsSpark \
     --num-executors 9 --executor-cores 16 --executor-memory 48G --driver-memory 32G \
     target/cs848-project-1.0-SNAPSHOT.jar \
@@ -34,7 +44,7 @@ spark-submit \
 
 : '
 spark-submit \
-    --name sent-detector-solr-spark \
+    --name solr-rdd-spark \
     --class ca.uwaterloo.SIGIR.SolrRddSpark \
     --num-executors 9 --executor-cores 16 --executor-memory 48G --driver-memory 32G \
     target/cs848-project-1.0-SNAPSHOT.jar \
@@ -46,6 +56,7 @@ spark-submit \
     --task $3
 '
 
+: '
 spark-submit \
     --name term-count \
     --class ca.uwaterloo.util.Count \
@@ -53,3 +64,4 @@ spark-submit \
     target/cs848-project-1.0-SNAPSHOT.jar \
     --input $1 \
     --output $2
+'
