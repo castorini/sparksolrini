1273fd474c35c7138911e5a2aa2ea926f157a38b
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-01-27 21:23:49 -0500

    Fix minor errors

--- src/main/scala/ca/uwaterloo/SIGIR/task/SleepTask.scala
+++ src/main/scala/ca/uwaterloo/SIGIR/task/SleepTask.scala
@@ -1,9 +1,7 @@
 package scala.ca.uwaterloo.SIGIR.task
 
-import ca.uwaterloo.Constants.MILLIS_IN_MINUTE
-
 class SleepTask(duration: Long) extends Task {
   def process(content: String) {
-    Thread.sleep(duration * MILLIS_IN_MINUTE)
+    Thread.sleep(duration)
   }
 }
