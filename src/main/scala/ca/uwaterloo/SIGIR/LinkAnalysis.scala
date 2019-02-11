1340e8f5eb40097f8f44088dd0740b3f0ca6906a
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-02-11 09:31:12 -0500

    Working version of network analysis

--- src/main/scala/ca/uwaterloo/SIGIR/LinkAnalysis.scala
+++ src/main/scala/ca/uwaterloo/SIGIR/LinkAnalysis.scala
@@ -1,11 +1,14 @@
 package ca.uwaterloo.SIGIR
 
+import scala.collection.JavaConverters._
+
 import java.nio.charset.StandardCharsets
 import java.net.URL
 import org.jsoup.Jsoup
 
 import ca.uwaterloo.conf.HdfsConf
 import ca.uwaterloo.util.Stemmer
+import com.google.common.net.InternetDomainName
 import nl.surfsara.warcutils.WarcInputFormat
 import org.apache.commons.io.IOUtils
 import org.apache.hadoop.io.LongWritable
@@ -35,19 +38,46 @@ object LinkAnalysis {
 
     val source_urls = sc.newAPIHadoopFile(path, classOf[WarcInputFormat], classOf[LongWritable], classOf[WarcRecord])
       .filter(pair => {
-        pair._2.header != null &&
-          pair._2.header.contentLengthStr != null &&
-          pair._2.header.contentTypeStr.equals("application/http;msgtype=response") &&
-        Stemmer.stem(IOUtils.toString(pair._2.getPayloadContent, StandardCharsets.UTF_8)).contains(Stemmer.stem(term))
+        pair._2.header != null && pair._2.header.contentLengthStr != null && pair._2.header.contentTypeStr.equals("application/http;msgtype=response")
       })
-      .flatMap(pair => {
-        val content = Stemmer.stem(IOUtils.toString(pair._2.getPayloadContent, StandardCharsets.UTF_8))
-        val pair_list = List[(String, String)]()
-        val links = Jsoup.parse(content).select("a[href]").attr("href")
-          for (link <- links) {
-            pair_list :+ (new URL(pair._2.header.warcTargetUriStr).getHost, link)
-          }
-        pair_list
-    })
+      .map(pair => {
+        try {
+          (InternetDomainName.from(new URL(pair._2.header.warcTargetUriStr).getHost.toString).topPrivateDomain().name(), IOUtils.toString(pair._2.getPayloadContent, StandardCharsets.UTF_8))
+        }
+        catch {
+          case e: Exception => println(e)
+            ("", "")
+        }
+      })
+      .filter(doc => !doc._1.isEmpty && Stemmer.stem(doc._2).contains(Stemmer.stem(term)))
+
+    val zipped_urls = source_urls
+      .sample(false, 0.01, 42)
+      .flatMap(record => {
+        val target_urls = Jsoup.parse(record._2)
+          .select("a[href]")
+          .asScala
+          .map(link => link.attr("abs:href"))
+          .filter(!_.isEmpty)
+          .map(link => {
+            try {
+              InternetDomainName.from(new URL(link).getHost).topPrivateDomain().name()
+            }
+            catch {
+              case e: Exception => println(e)
+                ""
+            }
+          })
+          .distinct
+          .take(3)
+
+        val src_host = (1 to target_urls.size).map(_ => record._1)
+        src_host zip target_urls
+      })
+      .distinct
+      .filter(x => x._1 != x._2)
+      .map(pair => pair._1 + ";" + pair._2)
+      .repartition(1)
+      .saveAsTextFile("hdfs://node-master:9000/links")
   }
 }
