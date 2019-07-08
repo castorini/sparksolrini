f4b66fc846d0f04fc87bfdd23e3723daca7d1735
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-07-08 16:19:44 -0400

    Add script to plot for KDE notebook

---
+++ demo/kde_hour.py
@@ -0,0 +1,28 @@
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+term = sys.argv[1]
+
+fig = plt.figure()
+
+with open('/tmp/kde/part-00000', 'r') as in_file:
+    densities = []
+    for line in in_file:
+        density = line[1:-2]
+        densities.append(density)
+
+hours = np.arange(0, 24)
+plt.plot(hours, densities, '-.', label=term)
+plt.xlabel('Hour')
+plt.ylabel('Density')
+plt.xticks(hours[1::3])
+plt.grid()
+
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
+           fancybox=True, shadow=True, ncol=4, prop={'size': 10})
+
+fig.savefig("kde.png")
+plt.show()
