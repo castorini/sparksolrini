74a1ae4af2b7ed2bfcae474a43e6e24b1a771aeb
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-07-08 23:42:50 -0400

    Update KDE plot script

--- demo/kde_day.py
+++ demo/kde_day.py
@@ -4,8 +4,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-term = sys.argv[1]
-
 fig = plt.figure()
 
 with open('/tmp/kde/part-00000', 'r') as in_file:
@@ -15,16 +13,14 @@ with open('/tmp/kde/part-00000', 'r') as in_file:
         densities.append(float(density))
 
 days = np.arange(0, 7)
-plt.plot(days, densities, '-.', label=term)
+plt.plot(days, densities, '-.')
 plt.xlabel('Day')
 plt.ylabel('Density')
 plt.xticks(days, ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'])
-plt.title('Term: {}'.format(term))
 plt.grid()
 plt.legend(prop={'size': 8})
 
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
            fancybox=True, shadow=True, ncol=4, prop={'size': 10})
 
-fig.savefig("kde.png")
-plt.show()
+fig.savefig("/tmp/kde/kde.png")
--- demo/kde_hour.py
+++ demo/kde_hour.py
@@ -4,8 +4,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-term = sys.argv[1]
-
 fig = plt.figure()
 
 with open('/tmp/kde/part-00000', 'r') as in_file:
@@ -13,13 +11,12 @@ with open('/tmp/kde/part-00000', 'r') as in_file:
     for line in in_file:
         density = line
         densities.append(float(density))
-        
+
 hours = np.arange(0, 24)
-plt.plot(hours, densities, '-.', label=term)
+plt.plot(hours, densities, '-.')
 plt.xlabel('Hour')
 plt.ylabel('Density')
 plt.xticks(hours[1::3])
-plt.title('Term: {}'.format(term))
 plt.grid()
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
                fancybox=True, shadow=True, ncol=4, prop={'size': 10})
@@ -27,5 +24,4 @@ plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
            fancybox=True, shadow=True, ncol=4, prop={'size': 10})
 
-fig.savefig("kde.png")
-plt.show()
+fig.savefig("/tmp/kde/kde.png")
